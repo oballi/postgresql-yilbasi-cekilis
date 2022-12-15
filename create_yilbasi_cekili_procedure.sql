a98f8883741e5abdad6f1706d5f6211b6cc1273c
Omer Faruk Balli <43172199+oballi@users.noreply.github.com>
2022-12-15 17:10:39 +0300

    Update create_yilbasi_cekili_procedure.sql

--- create_yilbasi_cekili_procedure.sql
+++ create_yilbasi_cekili_procedure.sql
@@ -11,17 +11,17 @@ BEGIN
 
     for r in select * from personel order by id
         loop
-            RAISE NOTICE 'Döngüden Gelen ID : % Gelen Ad %',r.id,r.ad_soyad;
+            --RAISE NOTICE 'Döngüden Gelen ID : % Gelen Ad %',r.id,r.ad_soyad;
 
             /* hediye alan kişi bulunur - random id üretilir */
             SELECT floor(random() * max(id) + 1)::int into random_id from personel;
-            RAISE NOTICE 'Hediye Alan Kiş İçin Random ID Üretildi : %',random_id;
+            --RAISE NOTICE 'Hediye Alan Kiş İçin Random ID Üretildi : %',random_id;
 
             /*Hediye Alan ID ile Hediye Veren ID birbirlerine eşit mi kontrol ediliyor..*/
             while random_id = r.id
                 loop
                     SELECT floor(random() * max(id) + 1)::int into random_id from personel;
-                    RAISE NOTICE 'Hediye veren ID ile hediye alan ID eşit. Yeni ID üretiliyor.. %',random_id;
+                    --RAISE NOTICE 'Hediye veren ID ile hediye alan ID eşit. Yeni ID üretiliyor.. %',random_id;
                 end loop;
 
             /*hediye alan kişi yilbasi tablosunda var mi kontrol et*/
@@ -33,7 +33,7 @@ BEGIN
                     while random_id = (select hediye_alan_id from yilbasi where hediye_alan_id = random_id)
                         loop
                             SELECT floor(random() * max(id) + 1)::int into random_id from personel;
-                            RAISE NOTICE 'Hediye alan ID daha önce yilbasi tablosunda kayitli. Tekrardan random id üretiliyor.. %',random_id;
+                            --RAISE NOTICE 'Hediye alan ID daha önce yilbasi tablosunda kayitli. Tekrardan random id üretiliyor.. %',random_id;
                         end loop;
                 end loop;
 
